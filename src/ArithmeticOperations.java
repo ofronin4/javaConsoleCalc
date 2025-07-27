0c2d9edfe4b63d375d5d001789eec7a51ddff49c
Oleksii Fronin <oleksiifronin@gmail.com>
2025-07-27 16:36:42 +0200

    Added logic for Digits class. New "ArithmeticOperations" class created with logic.

---
+++ src/ArithmeticOperations.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ArithmeticOperations {
+    Scanner scanner = new Scanner(System.in);
+public ArithmeticOperations(){
+    System.out.print("Choose Ariphmetic operation:\n1) Plus (+) \n2) Minus (-)\n3) Multiply (*)\n4) Divide (/)");
+    String operation = scanner.nextLine();
+
+}
+}
--- src/Digits.java
+++ src/Digits.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Digits {
+    public  void Digits(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first digit:");
+        double firstDigit = scanner.nextDouble();
+        System.out.print("Enter second digit:");
+        double secondDigit = scanner.nextDouble();
+
+    }
+
 }
