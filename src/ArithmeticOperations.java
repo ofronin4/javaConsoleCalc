77bc838377ad415c45845ed256f7ae887e270737
Oleksii Fronin <oleksiifronin@gmail.com>
2025-07-27 23:58:40 +0200

    Changed and added new code for Digits and ArithmeticOperations class.

--- src/ArithmeticOperations.java
+++ src/ArithmeticOperations.java
@@ -1,10 +1,70 @@
 import java.util.Scanner;
 
-public class ArithmeticOperations {
+public class ArithmeticOperations extends Digits {
+    private String result;
+    private String operation;
     Scanner scanner = new Scanner(System.in);
-public ArithmeticOperations(){
-    System.out.print("Choose Ariphmetic operation:\n1) Plus (+) \n2) Minus (-)\n3) Multiply (*)\n4) Divide (/)");
-    String operation = scanner.nextLine();
 
-}
+    public ArithmeticOperations() {
+        super();
+        System.out.print("Choose Arithmetic operation:\n1) Plus (+) \n2) Minus (-)\n3) Multiply (*)\n4) Divide (/)");
+        operation = scanner.nextLine();
+
+        switch (operation) {
+            case "1":
+            case "+":
+            case "Plus":
+            case "plus":
+                plus();
+                break;
+            case "2":
+            case "-":
+            case "Minus":
+            case "minus":
+                minus();
+                break;
+            case "3":
+            case "*":
+            case "Multiply":
+            case "multiply":
+                multiply();
+                break;
+            case "4":
+            case "/":
+            case "Divide":
+            case "divide":
+                divide();
+                break;
+            default:
+                result = "Invalid operation.";
+        }
+    }
+
+
+    // Operations
+    public void plus() {
+        result = String.valueOf(firstDigit + secondDigit);
+    }
+
+    public void minus() {
+        result = String.valueOf(firstDigit - secondDigit);
+    }
+
+    public void multiply() {
+        result = String.valueOf(firstDigit * secondDigit);
+    }
+
+    public void divide() {
+        if (secondDigit != 0) {
+            result = String.valueOf(firstDigit / secondDigit);
+        } else {
+            result = "Error: division by zero.";
+        }
+
+    }
+
+
+    public void Result() {
+        System.out.println("Result of " + operation + " of " + firstDigit + " and " + secondDigit + " is " + result +".");
+    }
 }
--- src/Digits.java
+++ src/Digits.java
@@ -1,13 +1,19 @@
 import java.util.Scanner;
 
 public class Digits {
-    public  void Digits(){
+    protected double firstDigit;
+    protected double secondDigit;
+
+    public void Digits(){
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter first digit:");
-        double firstDigit = scanner.nextDouble();
+        firstDigit = scanner.nextDouble();
         System.out.print("Enter second digit:");
-        double secondDigit = scanner.nextDouble();
+        secondDigit = scanner.nextDouble();
+
 
     }
 
 }
+
+
